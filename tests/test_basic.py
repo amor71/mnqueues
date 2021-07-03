3ebae5266abe4a8518b12adb51f7ca687a5ae9a3
Amichay Oren <amichay@sgeltd.com>
2021-07-03 18:33:29 +0300

    basic tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,5 +1,6 @@
 import pytest
 import mnqueues as mnq
+import multiprocessing
 
 
 def test_mqt_instantiate():
@@ -8,3 +9,27 @@ def test_mqt_instantiate():
 
 def test_create_monitor():
     monitor = mnq.Monitor("name")
+
+
+import pytest
+from multiprocessing import Queue, Process
+
+
+def producer(q: mnq.MNQueue):
+    print(q.get())
+
+
+def consumer(q: mnq.MNQueue):
+    q.put("testing 123")
+
+
+def test_mp_basic():
+    q = mnq.MNQueue()
+    p = Process(target=producer, args=(q,))
+    c = Process(target=consumer, args=(q,))
+
+    p.start()
+    c.start()
+
+    p.join()
+    c.join()
